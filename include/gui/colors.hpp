655ad0c616e474ab33e6c1b656b8720cf8e4c368
VoltZ <47382115+SuperSaiyajinVoltZ@users.noreply.github.com>
2019-11-19 20:16:08 +0100

    Initial Commit. :)

---
+++ include/gui/colors.hpp
@@ -0,0 +1,14 @@
+#ifndef COLORS_HPP
+#define COLORS_HPP
+
+#include <citro2d.h>
+
+#define BLACK 			C2D_Color32(0, 0, 0, 255)
+#define WHITE 			C2D_Color32(255, 255, 255, 255)
+#define RED				C2D_Color32(255, 0, 0, 255)
+#define GREEN			C2D_Color32(0, 255, 0, 255)
+#define BLUE			C2D_Color32(0, 0, 255, 255)
+
+typedef u32 Color;
+
+#endif
